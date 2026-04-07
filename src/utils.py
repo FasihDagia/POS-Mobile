5f9ebbef0c58c9ab4aedebec2fb1552e3fe0cd33
FasihDagia <fasihdagia2006@gmail.com>
2026-04-08 02:09:57 +0500

    created a utility function to create a treeview in windows

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from tkinter import ttk
+
 def center_window(root,width, height):
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
@@ -9,3 +12,45 @@ def center_window(root,width, height):
 def destroy_widgets(root):
     for widget in root.winfo_children():
         widget.destroy()
+
+def create_treeview(parent, columns, widths, height):
+
+    frame = ttk.Frame(parent)
+    frame.pack(fill="both", expand=True)
+
+    tree_scroll_y = ttk.Scrollbar(frame, orient="vertical")
+    tree_scroll_y.pack(side="right", fill="y")
+
+    tree_scroll_x = ttk.Scrollbar(frame, orient="horizontal")
+    tree_scroll_x.pack(side="bottom", fill="x")
+
+    tree = ttk.Treeview(
+        frame,
+        columns=columns,
+        show="headings",
+        yscrollcommand=tree_scroll_y.set,
+        xscrollcommand=tree_scroll_x.set,
+        height=height
+    )
+    tree.pack(fill=tk.X, padx=10)
+
+    tree_scroll_y.config(command=tree.yview)
+    tree_scroll_x.config(command=tree.xview)
+
+    for col, w in zip(columns, widths):
+        tree.heading(col, text=col)
+        tree.column(col, width=w, anchor="center")
+
+    tree.tag_configure("oddrow", background="#FFE5B4")   # light peach
+    tree.tag_configure("evenrow", background="#FFCBA4")  # darker peach
+
+    original_insert = tree.insert
+
+    def colored_insert(parent, index, values=None, **kwargs):
+        row_count = len(tree.get_children())
+        tag = "evenrow" if row_count % 2 == 0 else "oddrow"
+        return original_insert(parent, index, values=values, tags=(tag,), **kwargs)
+
+    tree.insert = colored_insert
+
+    return tree
