1c1faf4ba110ed68aa4877b6e997338bc19f6a68
FasihDagia <fasihdagia2006@gmail.com>
2026-04-17 18:21:27 +0500

    added an icon

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from src.windows import windows
+from src.utils import resource_path
 def main():
     root = tk.Tk()
     win = windows(root)
+    root.iconbitmap(resource_path("assets/point-of-sale.ico"))
     win.landing_page()
     root.mainloop()
 
